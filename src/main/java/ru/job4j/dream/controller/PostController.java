04b003d1a7f8980fef51d09114e6d95817f67de0
isuvorov1 <isuvorov1@outlook.com>
2022-03-04 17:55:11 -0500

    5. Список Вакансий [#504842]

--- src/main/java/ru/job4j/dream/controller/PostController.java
+++ src/main/java/ru/job4j/dream/controller/PostController.java
@@ -3,12 +3,13 @@ package ru.job4j.dream.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.dream.store.PostStore;
 import ru.job4j.dream.store.Store;
 
 @Controller
 public class PostController {
 
-    private final Store store = Store.instOf();
+    private final PostStore store = PostStore.instOf();
 
     @GetMapping("/posts")
     public String posts(Model model) {
