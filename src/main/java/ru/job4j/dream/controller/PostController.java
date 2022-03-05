538014d21e07576f81009d84ea1e02fe70ff2b95
isuvorov1 <isuvorov1@outlook.com>
2022-03-05 16:01:27 -0500

    8. HTML form. Создание кандидата. [#504845]

--- src/main/java/ru/job4j/dream/controller/PostController.java
+++ src/main/java/ru/job4j/dream/controller/PostController.java
@@ -19,7 +19,7 @@ public class PostController {
 
     @GetMapping("/addPost")
     public String addPost(Model model) {
-        model.addAttribute("post", new Post(0, "Заполните поле"));
+        model.addAttribute("posts", new Post(0, "Заполните поле"));
         return "addPost";
     }
 }
