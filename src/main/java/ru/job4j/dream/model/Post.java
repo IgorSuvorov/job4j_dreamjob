72c3df3add3effff2be0ca923c34d0c76e10c1fb
isuvorov1 <isuvorov1@outlook.com>
2022-03-28 01:36:48 -0400

    fixed

--- src/main/java/ru/job4j/dream/model/Post.java
+++ src/main/java/ru/job4j/dream/model/Post.java
@@ -5,13 +5,16 @@ import lombok.*;
 import java.io.Serializable;
 
 @NoArgsConstructor
+@RequiredArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode
 @ToString
 public class Post implements Serializable {
+    @NonNull
     private int id;
+    @NonNull
     private String name;
     private City city;
 }
